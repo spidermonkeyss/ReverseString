6d4ab916210c60e83f18988dc9fe8b53af5c52c6
ahill14 <andrewhill@columbus.rr.com>
2020-07-14 00:47:46 -0400

    Inital Commit

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,30 @@
+﻿//Reverse a String – Enter a string and the program will reverse it and print it out.
+using System;
+using Utlity;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = UserInput.GetUserStringInput();
+            char[] reverseString = new char[input.Length];
+            int endi = input.Length - 1;
+            foreach (char c in input)
+            {
+                reverseString[endi] = c;
+                endi--;
+            }
+
+            //Turn char array into string
+            string rs = "";
+            foreach (char c in reverseString)
+            {
+                rs += c;
+            }
+            Console.WriteLine("         Input:" + input + ".");
+            Console.WriteLine("Reverse String:" + rs + ".");
+        }
+    }
+}
---
+++ ReverseString/ReverseString/UserInput.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Utlity
+{
+    public class UserInput
+    {
+        public static int GetUserIntInput()
+        {
+            int intInput = 0;
+            string input = "";
+
+            bool isNumeric = false;
+            while(isNumeric != true)
+            {
+                input = Console.ReadLine();
+                bool isNumber = int.TryParse(input, out int n);
+                if (!isNumber)
+                {
+                    Console.WriteLine("Not a number");
+                    Console.WriteLine("Try again");
+                    isNumeric = false;
+                }
+                else
+                    isNumeric = true;
+            }
+            intInput = Convert.ToInt32(input);
+            return intInput;
+        }
+
+        public static float GetUserFloatInput()
+        {
+            float floatInput = 0;
+            string input = "";
+
+            bool isNumeric = false;
+            while (isNumeric != true)
+            {
+                input = Console.ReadLine();
+                bool isNumber = float.TryParse(input, out float n);
+                if (!isNumber)
+                {
+                    Console.WriteLine("Not a number");
+                    Console.WriteLine("Try again");
+                    isNumeric = false;
+                }
+                else
+                    isNumeric = true;
+            }
+            floatInput = (float)Convert.ToDouble(input);
+            return floatInput;
+        }
+
+        public static string GetUserStringInput()
+        {
+            string stringInput = "";
+
+            stringInput = Console.ReadLine();
+
+            return stringInput;
+        }
+    }
+}
